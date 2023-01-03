f8e195c67f9d5064f783d383ed3f285a86cd4a85
devilarmaan! <devilarmaan63@gmail.com>
2023-01-03 20:55:33 +0600

    add one file

---
+++ array_operations.js
@@ -0,0 +1,36 @@
+// Declarative traverse
+const numbers = [2, 5, 6, 7, 89, 100];
+
+numbers.forEach( function () {
+    console.log('Hello');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
