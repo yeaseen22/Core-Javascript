3de7084e0b9b9b1ecb69e84a07a670f50ddc1d8f
devilarmaan! <devilarmaan63@gmail.com>
2023-01-23 22:28:05 +0600

    some changes in objectBasic file

--- objectBasic.js
+++ objectBasic.js
@@ -15,6 +15,51 @@ const microphone = {
     }
 }
 
+/** 
+ * there are two different parts of object
+ * 1. noun/adjective (state/data/property/field)
+ * 2. verb / (functionalities => start, stop)
+ * */ 
+
+
+const testObj = new Object();
+testObj.name = 'test Object';
+testObj.time = new Date();
+// console.log(testObj);
+
+
+
+// object have prototype methods and without prototype base(literator object)
+
+// console.log(Object.keys(microphone));
+// console.log(Object.values(microphone));
+
+// object dot notation microphone.brand
+// object array notation microphone['brand']
+
+
+// for(let k in microphone) {
+//     let name = 66
+//     console.log(k, '\n', microphone['name']);
+// }
+
+// object key value pair(its return an 2d array)
+
+console.log(Object.entries(microphone));
+
+const array = [
+    [ 'brand', 'fiften' ],
+    [ 'indicator', true ],
+    [ 'price', 899 ],
+    [ 'color', 'black' ],
+  ];
+
+
+//   return array to object
+console.log(Object.fromEntries(array));
+  
+
+
 
 
 
