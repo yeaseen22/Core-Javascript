51612c2a09791df8d3ef7cbc2ba946185fee3a23
devilarmaan! <devilarmaan63@gmail.com>
2023-01-24 14:05:06 +0600

    update

---
+++ arrayOfObject.js
@@ -0,0 +1,37 @@
+/**
+ * store 10 student information using array and object
+ * name
+ * email
+ */
+
+
+
+// Manually create random ID
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxx'.replace(/[xy]/g, (c) => {
+		const r = (Math.random() * 16) | 0;
+		const v = c = 'x' ? r : (r & 0x3) | 0x8;
+		return v.toString(16);
+	});
+}
+
+
+const student = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+
