6b35de4dceda45e4518e60cce1a9f66b4716fa21
devilarmaan! <devilarmaan63@gmail.com>
2023-02-18 20:51:21 +0600

    some code added

--- objectBasic.js
+++ objectBasic.js
@@ -45,19 +45,77 @@ testObj.time = new Date();
 
 // object key value pair(its return an 2d array)
 
-console.log(Object.entries(microphone));
+// console.log(Object.entries(microphone));
 
-const array = [
-    [ 'brand', 'fiften' ],
-    [ 'indicator', true ],
-    [ 'price', 899 ],
-    [ 'color', 'black' ],
-  ];
+// const array = [
+//     [ 'brand', 'fiften' ],
+//     [ 'indicator', true ],
+//     [ 'price', 899 ],
+//     [ 'color', 'black' ],
+//   ];
 
 
 //   return array to object
-console.log(Object.fromEntries(array));
+// console.log(Object.fromEntries(array));
   
+
+// const empty = {};
+
+// console.log(Boolean(empty));
+
+
+const students = [
+    {
+        id: '67de71e5-0eac-474f-ab51-850ba9b31ed5',
+        name: 'Md Al-Amin',
+        email: 'alamin@test.com',
+    },
+    {
+        id: 'ebdf6b78-c32b-4b1d-8574-e8c655b05c1e',
+        name: 'Akib Ahmed',
+        email: 'akib@test.com',
+    },
+    {
+        id: 'ee729e84-a84e-4adf-b32c-4647a7114d5b',
+        name: 'Elias Emon',
+        email: 'elias@test.com',
+    },
+];
+
+// update data
+const idToUpdate = 'ee729e84-a84e-4adf-b32c-4647a7114d5b';
+const updatedData = {
+    name: 'Habiba Akhtar',
+    email: 'habiba@test.com',
+};
+
+const updatedIndex = students.findIndex( (item) => item.id === idToUpdate);
+students[updatedIndex] = {
+    ...students[updatedIndex],
+    ...updatedData
+};
+
+// delete 
+const deleteStudentObj = students.splice( updatedIndex, 1);
+console.log(deleteStudentObj);
+
+
+console.log(students);
+
+// object tranverse
+for(let i in students) {
+    console.log(students[i].name);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
