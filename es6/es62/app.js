d81decf2a435f9ed46a55cb68b616d08f87751ac
devilarmaan! <devilarmaan63@gmail.com>
2023-04-04 15:10:06 +0600

    practice generator

--- es6/es62/app.js
+++ es6/es62/app.js
@@ -18,11 +18,11 @@ function createIterator(collection){
     }
 }
 
-let iterate = createIterator(arr);
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
+// let iterate = createIterator(arr);
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
 // console.dir(iterate.next)
 
 // we can use iterator with this also
@@ -30,17 +30,97 @@ console.log(iterate.next());
 // console.log('str'[Symbol.iterator]);
 
 
-let iterate2 = arr[Symbol.iterator]();
+// let iterate2 = arr[Symbol.iterator]();
 
-console.log(iterate2.next());
-console.log(iterate2.next());
-console.log(iterate2.next());
-console.log(iterate2.next());
+// console.log(iterate2.next());
+// console.log(iterate2.next());
+// console.log(iterate2.next());
+// console.log(iterate2.next());
 
 
-let str = 'Text';
-let iterateText = str[Symbol.iterator]();
-console.log(iterateText.next());
-console.log(iterateText.next());
-console.log(iterateText.next());
-console.log(iterateText.next());
+// let str = 'Text';
+// let iterateText = str[Symbol.iterator]();
+// console.log(iterateText.next());
+// console.log(iterateText.next());
+// console.log(iterateText.next());
+// console.log(iterateText.next());
+
+// for of loop
+// for(let v of arr){
+//     console.log(v);
+// }
+
+// for(let v of 'HM Nayem'){
+//     console.log(v);
+// }
+
+let objeForOfloop = {
+    a:10,
+    b:20
+}
+
+// it not work for object(becase this object is not iterable)
+for(let v of objeForOfloop){
+    // console.log(v);
+}
+
+for(let i in objeForOfloop){
+    // console.log(i);
+}
+
+
+// Generators in js
+
+let objCustoItarable = {
+    start: 1,
+    end: 10,
+
+    // [Symbol.iterator]: function (){
+    //     let currentValue = this.start;
+    //     const self = this;
+    //     return {
+    //         next() {
+    //             return {
+    //                 done: currentValue > self.end,
+    //                 value: currentValue > self.end ? undefined : currentValue++
+    //             }
+    //         }
+    //     }
+    // }
+
+    // implement generator
+//     [Symbol.iterator]: function* () {
+//         let currentValue = this.start;
+//         while(currentValue <= this.end){
+//             yield currentValue++;
+//         }
+//     }
+ }
+
+// function* generate(){
+//     yield 1
+//     yield 2
+//     yield 3
+// }
+
+function* generate2(collection){
+    for(let i = 0; i < collection.length; i++){
+        yield collection[i];
+    }
+}
+
+let iterate3 = generate(arr)
+console.log(iterate3.next());
+
+
+
+
+
+
+
+
+
+
+
+
+
