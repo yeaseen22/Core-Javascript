b2452a22f829515a4c282f96bc0928d47badd2e8
devilarmaan! <devilarmaan63@gmail.com>
2023-04-13 22:48:35 +0600

    hello

--- functions.js
+++ functions.js
@@ -16,4 +16,21 @@ function showMessage(from, text = "no text given") {
     console.log( from + ": " + text );
   }
   
-  showMessage("Ann",);
+  // showMessage("Ann",);
+
+  // javascript reflection
+const greeting = new Function (
+  'name',
+  'email',
+
+  `const fName = name.split(' ')[0];
+   console.log("Hello,", fName, "Is that your email?", email);
+   console.log("Yah This Is Mine");
+   return fName;
+  `
+  )
+
+  console.log(typeof greeting);
+  const fName = greeting('Hm Nayem', 'hmnayme@gmail.com');
+  console.log('First Name', fName);
+
