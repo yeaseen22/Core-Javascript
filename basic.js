d8f0b399eacecf879155d8ed605c8c318d692244
devilarmaan! <devilarmaan63@gmail.com>
2023-05-04 12:31:23 +0600

    basic file add

---
+++ basic.js
@@ -0,0 +1,20 @@
+
+const fruits = ["banana", "apple", "peach"]
+
+for(let i = 0; i < fruits.length; i++) {
+    // console.log(fruits[i]);
+}
+
+let str = "Hello";
+// console.log(str.charAt(0));
+// console.log(str.length);
+// console.log(str.toUpperCase());
+
+const myString = "Hello, World!";
+const myArray = myString.split(", ")
+console.log(myArray);
+
+const upperCaseString = myString.toUpperCase();
+console.log(upperCaseString);
+
+console.log(isNaN(str));
