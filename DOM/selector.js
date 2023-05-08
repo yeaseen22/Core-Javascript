091b4020fcb287f077f646f25076aa34dd5afdc3
devilarmaan! <devilarmaan63@gmail.com>
2023-05-08 12:34:09 +0600

    dom knowledge

--- DOM/selector.js
+++ DOM/selector.js
@@ -1,16 +1,28 @@
 // id, class, TagName, Name
 
 let title = document.getElementById('title')
-console.log(title);
+// console.log(title);
 
 let paragraph = document.getElementsByClassName('pera')
-console.log(paragraph);
+// console.log(paragraph);
 
 let list = document.getElementsByName('li')
-console.log(list);
+// console.log(list);
 
 let listItemOne = document.getElementsByName('list-item-one ');
-console.log(listItemOne);
+// console.log(listItemOne);
+
+let title2 = document.querySelector('#title')
+console.log(title2);
+
+let paragraph2 = document.querySelectorAll('.pera');
+console.log(paragraph2);
+
+let list2 = document.querySelectorAll('li');
+console.log(list2);
+
+let listItemOne2 = document.querySelector('[name="list-item-one"]')
+console.log(listItemOne2);
 
 
 
