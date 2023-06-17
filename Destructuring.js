a98869d4d75ed275e6070d9f29c77b822dacd6f2
devilarmaan <devilarmaan63@gmail.com>
2023-06-18 00:39:09 +0600

    added

--- Destructuring.js
+++ Destructuring.js
@@ -56,4 +56,94 @@ const [, secondHobbies] = user.hobbies;
 const getUserInfo = ({ names, email }) => {
   console.log(names);
 };
-getUserInfo(user);
+// getUserInfo(user);
+
+const ProductService = (() => {
+  const products = [
+    { id: 1, name: "iPhone", price: 999 },
+    { id: 2, name: "Samsung Galaxy", price: 899 },
+    { id: 3, name: "Google Pixel", price: 799 },
+  ];
+
+  const addProduct = ({ id, name, price }) => {
+    const product = { id, name, price };
+    products.push(product);
+    console.log(`Product added: ${product.name}`);
+  };
+
+  const updateProduct = ({ id, ...rest }) => {
+    const product = products.find((p) => p.id === id);
+
+    if (product) {
+      Object.assign(product, rest);
+      console.log(`Product updated: ${product.name}`);
+    } else {
+      console.log("Product not found");
+    }
+  };
+
+  const deleteProduct = (id) => {
+    const index = products.findIndex((p) => p.id === id);
+
+    if (index !== -1) {
+      const deletedProduct = products.splice(index, 1)[0];
+      console.log(`Product deleted: ${deletedProduct.name}`);
+    } else {
+      console.log("Product not found");
+    }
+  };
+
+  const getProduct = (id) => {
+    const product = products.find((p) => p.id === id);
+
+    if (product) {
+      console.log(`Product details for ID ${id}:`, product);
+    } else {
+      console.log("Product not found");
+    }
+  };
+
+  return {
+    addProduct,
+    updateProduct,
+    deleteProduct,
+    getProduct,
+  };
+})();
+
+// Usage
+const newProduct = { id: 4, name: "Sony Xperia", price: 699 };
+ProductService.addProduct(newProduct);
+
+const updatedProduct = { id: 2, price: 799 };
+ProductService.updateProduct(updatedProduct);
+
+ProductService.deleteProduct(3);
+
+ProductService.getProduct(1);
+
+// Application to calculate the total price of products
+
+// Function to calculate the total price of products
+function calculateTotalPrice(...products) {
+  // Spread operator is used to pass an indefinite number of products as arguments
+
+  // Calculate the total price
+  const totalPrice = products.reduce((sum, product) => sum + product.price, 0);
+
+  // Return the total price
+  return totalPrice;
+}
+
+// Sample products
+const products = [
+  { name: "iPhone", price: 999 },
+  { name: "Samsung Galaxy", price: 899 },
+  { name: "Google Pixel", price: 799 },
+];
+
+// Calculate the total price using spread operator
+const totalPrice = calculateTotalPrice(...products);
+
+// Display the total price
+console.log(`Total Price: $${totalPrice}`);
