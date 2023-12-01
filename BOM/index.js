c35fe684241e5bab0fa6ec4c3279dce92118a70d
devilarmaan <devilarmaan63@gmail.com>
2023-12-01 17:09:27 +0600

    MASTER: work with BOM

--- BOM/index.js
+++ BOM/index.js
@@ -7,6 +7,12 @@ const availHeight = document.getElementById("availHeight");
 const colorDepth = document.getElementById("colorDepth");
 const pixelDepth = document.getElementById("pixelDepth");
 
+// for window location
+const href = document.getElementById("href");
+const hostname = document.getElementById("hostname");
+const protocol = document.getElementById("protocol");
+const port = document.getElementById("port");
+
 // show window object properties
 width.innerHTML = "window inner width is:" + window.innerWidth;
 height.innerHTML = "window inner height is:" + window.innerHeight;
@@ -21,4 +27,25 @@ function openWindow() {
 
 function closeWindow() {
   myWindow.close();
+}
+
+href.innerHTML = "Window href" + location.href;
+hostname.innerHTML = "Window hostname" + location.hostname;
+protocol.innerHTML = "Window protocol" + location.protocol;
+port.innerHTML = "Window  port" + location.port;
+
+function loadw3s() {
+  window.location.assign("https://www.w3schools.com");
+}
+
+/**
+ * for history
+ */
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward();
 }
