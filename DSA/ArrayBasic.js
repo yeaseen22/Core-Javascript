a78d76603c371d5e2a79049b63347295110dd911
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2024-12-04 14:39:29 +0600

    ds another problem solve

--- DSA/ArrayBasic.js
+++ DSA/ArrayBasic.js
@@ -39,3 +39,43 @@ function insertElement(arr, n, x, pos)
     }
     arr[pos] = x;
 }
+
+
+/**
+ * FINDING THE SECOND LARGEST VALUE IN AN ARRAY
+ *  Made with Efficient Way of Solving this problem
+ *  Solved with O(1) time complexity | O(n) space complexity
+ *  Solve with Single Loop
+ * @param {*} myArr 
+ * @returns 
+ */
+function findSecondLargestValue2(myArr) {
+    const arrSize = myArr.length;
+    const lastIndex = arrSize - 1;
+
+    // initilized supposed secondMax and maxValue
+    let secondMax = myArr[0];
+    let maxVal = myArr[lastIndex];
+
+    // loop through the array
+    // compare the value with max value, if the value is greater than max value
+    // then assign the value to the max value and assign the max value to the second max value
+    for (let i = 0; i < arrSize; i++) {
+        if (myArr[i] > maxVal) {
+            secondMax = maxVal; // 5, 5 = 5
+            maxVal = myArr[i]; // 5, 7 = 7
+
+            // if the value is greater than second max value and less than max value
+            // then assign the value to the second max value
+        } else if (myArr[i] > secondMax && myArr[i] < maxVal) {
+            secondMax = myArr[i]; // 5
+        }
+    }
+
+    return secondMax;
+}
+
+// Test Cases to Run..
+const myArr = [3, 5, 7, 1, 4];
+const result = findSecondLargestValue2(myArr);
+console.log('Second Max - ', result);
