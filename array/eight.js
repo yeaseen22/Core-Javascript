7f4602317505d2d782cbb4aadab9c6e9c7c46cd4
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2025-04-06 13:46:04 +0600

    stack practice

---
+++ array/eight.js
@@ -0,0 +1,40 @@
+/*
+*@Title implementing a basic stack 
+*/
+
+MAX_SIZE = 30;
+class Stack{
+    constructor(){
+        this.list = new Array(MAX_SIZE)
+        this.top = -1;
+    }
+    push(item){
+        if(this.top >= MAX_SIZE){
+            console.log("Stack is overflow");
+            return false;
+        }
+        this.list[++this.top] = item;
+        return true;
+    }
+    pop(){
+        if(this.isEmpty()){
+            console.log("Stack is underflow");
+            return false;
+        }
+        let item = this.list[this.top]
+        delete this.list[this.top]
+        this.top--;
+        return item;
+    }
+    peek(){
+        if(this.isEmpty()){
+            console.log("Stack is underflow");
+            return false;
+        }
+        return this.list[this.top]
+    }
+    isEmpty(){
+        return this.top < 0;
+    }
+}
+
