cf1b82ec7b1c4a64ebb489f7448609edc7fb23f4
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2025-05-07 11:17:59 +0600

    js practice

--- array/index.js
+++ array/index.js
@@ -51,6 +51,54 @@ class CustomArray {
         this.length--;
         return element;
     }
+
+    get2(index) {
+        return this.array[index]
+    }
+
+    #lenghtCheck2(index) {
+        if (index < 0 || index > this.length) {
+            throw new Error('Error boundary')
+        }
+    }
+
+    #shrink(){}
+
+    grow(){}
+
+    push2(element) {
+        this.array[this.length] = element;
+        this.length++;
+    }
+
+    insert2(index, element) {
+        this.#lenghtCheck2(index)
+
+        for (let i = this.length; i > index; i--) {
+            this.array[i] = this.array[i - 1]
+        }
+        this.array[index] = element;
+        this.length++;
+    }
+
+    remove2(index) {
+
+        this.#lenghtCheck2(index)
+
+        for (let i = index; i < this.length - 1; i++) {
+            this.array[i] = this.array[i + 1]
+        }
+        this.length--
+    }
+
+    update2(index, element) {
+        this.array[index] = element;
+    }
+
+    pop2() {
+        this.array[this.length - 1] = undefined;
+        this.length--;
+    }
 }
 
 const newCustom = new CustomArray();
@@ -59,7 +107,7 @@ newCustom.push(2)
 newCustom.push(4)
 // newCustom.insert(1, 30)
 // newCustom.remove(0)
-newCustom.pop()
+newCustom.pop2()
 console.log(newCustom);
 
 
---
+++ promise/app3.js
@@ -0,0 +1,107 @@
+
+// callback
+function showMessage() {
+    console.log('result is out');
+
+}
+
+// callback hell
+// Inversion of controll (passing responsivility and it not fulfilled)
+
+function giveExam(cb) {
+    console.log('exam giving');
+    cb() // if cb not calling it's making inversion of controll
+}
+
+// giveExam(showMessage)
+
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Done');
+        resolve('Promise done')
+    }, 1000)
+})
+
+// console.log('the promise', p1);
+
+// p1.then((data) => {
+//     console.log('from then', data);
+// return Promise.resolve() by default
+// return 2 it make Promise.resolve(2) note: then catch finally all return resolved promise for chaining
+
+// }).finally(() => {
+//     console.log('Finally');
+
+// })
+
+// console.log('promise', p1);
+
+const state = {
+    PENDING: 'PENDING',
+    FULFILLED: "FULFILLED",
+    REJECTED: "REJECTED"
+}
+class MyPromise {
+    #promiseState = state.PENDING;
+    #handlers = [];
+    constructor(callback) {
+        callback(this.#resolve, this.#resolve)
+    }
+
+    #resolve = (value) => {
+        this.#promiseState = state.FULFILLED;
+
+        if (this.#promiseState === state.FULFILLED) {
+            this.#handlers.forEach((fns) => {
+                fns(value)
+            })
+        }
+    }
+    #reject() { }
+    then(thencallback) {
+        this.#handlers.push(thencallback)
+    }
+    catch() { }
+    finally() { }
+}
+
+const promiseObj = new MyPromise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve('Time form my promise');
+    }, 3000)
+})
+
+promiseObj.then((data) => {
+    console.log('data from resolve', data);
+    console.log('i am inside then block');
+})
+
+console.log(promiseObj);
+
+/**
+ * 1 then catch finally return a resolved promise that's why we can chain it
+ * 2 you can have multiple then catch finally
+ * 3 only first catch execute other ignored
+ * 4 you can pass a second parameter to then block which act as a catch function priority will give to this argument other catch
+ * 5 if inside chain. after catch any other then method throw error in this case nearest catch block will get execute
+ */
+
+
+function createAsyncTasks(data, time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            resolve(data)
+        }, time)
+    })
+}
+
+const pFlavour1 = createAsyncTasks('p1', 100)
+const pFlavour2 = createAsyncTasks('p2', 400)
+const pFlavour3 = createAsyncTasks('p3', 100)
+
+// to make all promise use in build method
+Promise.all([pFlavour1,pFlavour2,pFlavour3]).then((data) => {  // no performance benefits
+    console.log('form promise all',data);
+    
+})
