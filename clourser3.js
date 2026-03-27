1d6bb1b49974c9489a258d5fe54edec0929198fc
yeaseen22 <devilarmaan63@gmail.com>
2026-03-27 23:45:29 +0600

    update clouser3

--- clourser3.js
+++ clourser3.js
@@ -29,3 +29,31 @@ function outerCount() {
 
 let returnValue = outerCount();
 returnValue()
+
+// real world example
+
+function createBankAccount(initialBalance) {
+    let balance = initialBalance;
+
+
+    return {
+        "deposit": (amount) => {
+            balance = balance + amount;
+
+            console.log('Deposit', amount, 'cureentBalance', balance);
+
+        },
+
+        "widraw": (amount) => {
+            balance = balance - amount;
+
+            console.log('Widraw', amount, 'courrentBalance', balance);
+
+        }
+    }
+}
+
+const FirstUser = createBankAccount(100)
+// console.log(FirstUser(300));
+console.log(FirstUser.deposit(300));
+
