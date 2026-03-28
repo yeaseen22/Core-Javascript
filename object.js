7202335763bfa7e9be1004bab44f82965b2b3fff
yeaseen22 <devilarmaan63@gmail.com>
2026-03-28 12:53:39 +0600

    update

---
+++ object.js
@@ -0,0 +1,13 @@
+
+let user = {
+    name: "tapas",
+    age: 10,
+    "is admin": true
+}
+
+user.isSceniorCitizen = false;
+user["movie lover"] = false;
+
+console.log('user object', user.isSceniorCitizen);
+console.log('user', user["is admin"])
+
